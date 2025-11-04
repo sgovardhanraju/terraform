9d39dcd5c74ae9dbe3af0189417e5738e117ce8a
Govardhan Raju <gsurapur@gmail.com>
2025-11-04 18:46:20 +0400

    session-30

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,25 +1,29 @@
 resource "aws_instance" "terraform" {
-  ami = var.ami_id
-  instance_type = var.instance_type
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
-  tags = var.ec2_tags
+
+  tags = {
+    Name = "terraform"
+    Terraform = "true"
+  }
 }
 
 resource "aws_security_group" "allow_all" {
-    name = var.sg_name
+    name = "allow-all"
 
     egress {
-        from_port = var.egress_from_port
-        to_port = var.egress_to_port
-        protocol = var.egress_protocol
-        cidr_blocks = var.cidr
+        from_port = 0 # from port 0 to port 0 mean all ports
+        to_port = 0
+        protocol = "-1" # means all protocols
+        cidr_blocks = ["0.0.0.0/0"] # internet
     }
 
     ingress {
-        from_port = var.ingress_from_port
-        to_port = var.ingress_to_port
-        protocol = var.ingress_protocl
-        cidr_blocks = var.cidr
+        from_port = 0 # from port 0 to port 0 mean all ports
+        to_port = 0
+        protocol = "-1" # -1 means all protocols
+        cidr_blocks = ["0.0.0.0/0"] # internet
     }
 
     tags = {

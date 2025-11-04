826e92f0224c3d0323d6184d168a39353511d089
Govardhan Raju <gsurapur@gmail.com>
2025-11-04 21:23:58 +0400

    session-31

--- for-loop/variables.tf
+++ for-loop/variables.tf
@@ -2,7 +2,7 @@ variable "instances" {
   # default = ["mongodb", "redis","mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend"]
   default = {
     mongodb = "t3.micro"
-    resid = "t3.micro"
+    redis = "t3.micro"
     mysql = "t3.small"
   }
 }

3a463940a4a6d6619063b065e6f0c351858dd5a3
Govardhan Raju <gsurapur@gmail.com>
2025-11-04 15:23:35 +0400

    session-29

--- ec2.tf
+++ ec2.tf
@@ -7,3 +7,26 @@ resource "aws_instance" "terraform" {
     Terraform = "true"
   }
 }
+
+resource "aws_security_group" "allow_all" {
+    name = "allow_all"
+
+    egress {
+        from_port = 0 # from port 0 to port 0 mean all ports
+        to_port = 0
+        protocol = "-1" # -1 means all protocols
+        cidr_blocks = ["0.0.0.0/0"] # internet
+    }
+
+    ingress {
+        from_port = 0 # from port 0 to port 0 mean all ports
+        to_port = 0
+        protocol = "-1" # -1 means all protocols
+        cidr_blocks = ["0.0.0.0/0"] # internet
+    }
+
+    tags = {
+      Name = "allow_all"
+    }
+  
+}

0954df46c70c92e5b64ec3b531169c56851e6af2
Govardhan Raju <gsurapur@gmail.com>
2025-11-05 19:20:50 +0400

    session-31

---
+++ data-source/data.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "joindevops" {
+    owners = ["973714476881"]
+    most_recent = true
+
+    filter {
+      name = "name"
+      values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+      name ="root-device-type"
+      values = ["ebs"]
+    }
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+    }
+}
+
+output "ami_id" {
+    value = data.aws_ami.joindevops.id
+}
+
+data "aws_instance" "mongodb" {
+    instance_id = "i-0ccd8bd5f09e1d90d"
+}

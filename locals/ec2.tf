793216d8e58eb9224d6c1cd7a2b55faf5cd5eb36
Govardhan Raju <gsurapur@gmail.com>
2025-11-08 12:52:25 +0400

    session-32

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform" {
   ami = var.ami_id
-  instance_type = var.instance_type
+  instance_type = local.instance_type
   vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = var.ec2_tags
 }

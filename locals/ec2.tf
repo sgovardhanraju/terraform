7f1be1d7013f6d8608810045dbf41de84c5f0e48
Govardhan Raju <gsurapur@gmail.com>
2025-11-08 12:48:09 +0400

    session-32

---
+++ locals/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "terraform" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  tags = var.ec2_tags
+}
+
+resource "aws_security_group" "allow_all" {
+    name = var.sg_name
+
+    egress {
+        from_port = var.egress_from_port
+        to_port = var.egress_to_port
+        protocol = var.egress_protocol
+        cidr_blocks = var.cidr
+    }
+
+    ingress {
+        from_port = var.ingress_from_port
+        to_port = var.ingress_to_port
+        protocol = var.ingress_protocl
+        cidr_blocks = var.cidr
+    }
+
+    tags = {
+      Name = "allow-all"
+    }
+  
+}
